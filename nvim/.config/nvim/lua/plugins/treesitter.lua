9abd4d15c307fe3f462ad5d9686f830a60aa2a1a
gcd418 <patosquare418@gmail.com>
2025-03-19 14:08:05 -0400

    feat: nvim configuration

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,15 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = false,
-        ensure_installed = {
-          "bash",
-          "ruby",
-          "html",
-          "css",
-          "scss",
-          "javascript",
-          "typescript",
-          "json",
-          "lua",
-        },
-        highlight = { enable = true },
-        indent = { enable = false },
-      })
-    end
-  }
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {"lua", "javascript", "cpp", "bash", "arduino", "hyprlang"},
+      highlight = { enable = true },
+      indent = { enable = true},
+    })
+    vim.filetype.add({
+      pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+    })
+  end
 }
