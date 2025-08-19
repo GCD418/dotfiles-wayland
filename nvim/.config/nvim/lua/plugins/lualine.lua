3651444edf911dfd93b526ce6085fe544dc348ec
gabriel <patosquare418@gmail.com>
2025-08-19 18:11:23 -0400

    fix: now I can see when recording macros and keys that had been pressed

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,33 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    require('lualine').setup({
+    require("lualine").setup({
       options = {
-        theme = 'dracula'
-      }
+        theme = "dracula",
+      },
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.status.message.get_hl,
+            cond = require("noice").api.status.message.has,
+          },
+          {
+            require("noice").api.status.command.get,
+            cond = require("noice").api.status.command.has,
+            -- color = { fg = "#ff9e64" },
+          },
+          -- {
+          --   require("noice").api.status.mode.get,
+          --   cond = require("noice").api.status.mode.has,
+          --   color = { fg = "#ff9e64" },
+          -- },
+          {
+            require("noice").api.status.search.get,
+            cond = require("noice").api.status.search.has,
+            -- color = { fg = "#ff9e64" },
+          },
+        },
+      },
     })
-  end
+  end,
 }
